74921ab837c95280751fcd4de01e1385a57cebbf
Adan Jauregui <chido1631@gmail.com>
2018-11-07 08:31:34 -0700

    Added sample runner main window

--- TiempoDev.DataTypeSamples/Collections.cs
+++ TiempoDev.DataTypeSamples/Collections.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using TiempoDev.DataTypeSamples.Generic;
 
 namespace TiempoDev.DataTypeSamples
 {
--- TiempoDev.DataTypeSamples/Program.cs
+++ TiempoDev.DataTypeSamples/Program.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using TiempoDev.DataTypeSamples.Utils;
 
 namespace TiempoDev.DataTypeSamples
 {
     public class Program
     {
+        internal static readonly TerminalFormatter _formatter;
+
+        static Program()
+        {
+            try
+            {
+                TerminalFormatter.EnableWindowsVirtualTerminalSequences();
+            }
+            catch (Exception)
+            {
+                // Ignore
+            }
+
+            try
+            {
+                TerminalFormatter.EnableWindowsVirtualTerminalSequences(stdErr: true);
+            }
+            catch (Exception)
+            {
+                // Ignore
+            }
+
+            _formatter = new TerminalFormatter();
+        }
+
         static void Main(string[] args)
         {
+            try
+            {
+                SampleRunner.Build(_formatter)
+                    .Register<PrimitiveTypes>("Primitive types (int, bool, char, etc.)")
+                    .Register<Collections>("Collections (generic lists, array lists and arrays)")
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error has ocurred: {ex.Message}");
+            }
 
+            //Thread.Sleep(TimeSpan.FromSeconds(5));
         }
     }
 }
